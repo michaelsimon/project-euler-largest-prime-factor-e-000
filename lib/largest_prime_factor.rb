0ac21d5f315aeaf5253b17f31cee6dbe1a80e2bc
michaelsimon <michael.simon@gmail.com>
2015-12-23 00:27:31 -0500

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,15 @@
 # Enter your procedural solution here!
+require 'prime'
+def largest_prime_factor(input)
+primes = Array.new
+
+  Prime.each do |prime_num|
+    if prime_num < input 
+      if input % prime_num == 0
+        primes << prime_num
+      end
+    else break
+    end
+  end    
+  return primes.last
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,28 @@
 # Enter your object-oriented solution here!
+require 'prime'
+
+class LargestPrimeFactor
+attr_accessor :maxnum
+
+def initialize(maxnum)
+  @maxnum = maxnum
+end
+
+
+def number
+primes = Array.new
+
+  Prime.each do |prime_num|
+    if prime_num < @maxnum 
+      if @maxnum % prime_num == 0
+        primes << prime_num
+      end
+    else break
+    end
+  end    
+  return primes.last
+end
+
+end
+
+
